c33db1f5d44ae0d2f85c16b59e05d728dbb0e95a
Andre Alves Leocadio <andrealvesleocadio17@gmail.com>
2025-03-16 03:05:45 -0300

    feat: add totp and credentials a manual input and output

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hiweus/LockBox
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/hiweus/LockBox/totp"
+)
+
+const credentialFile = "credentials.json"
+
+type Credential struct {
+	Content string `json:"content"`
+	Type    string `json:"type"`
+	Key     string `json:"key"`
+}
+
+func (c Credential) GetContent() string {
+	if c.Type == "totp" {
+		return fmt.Sprintf("%d", totp.GenerateTOTP(c.Content, 6, time.Now().Unix()))
+	}
+
+	return c.Content
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
+}
+
+func main() {
+
+	if len(os.Args) < 2 {
+		log.Fatal("Missing required argument")
+	}
+
+	credentialKey := os.Args[1]
+
+	if !fileExists(filepath.Join(".", credentialFile)) {
+		fmt.Println("First time running, creating credentials file")
+		os.WriteFile(filepath.Join(".", credentialFile), []byte("[]"), 0644)
+	}
+
+	credentialFileInput, err := os.ReadFile(filepath.Join(".", credentialFile))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var credentials []Credential
+	err = json.Unmarshal(credentialFileInput, &credentials)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var expectedCredential *Credential
+	for _, credential := range credentials {
+		if credential.Key == credentialKey {
+			expectedCredential = &credential
+			break
+		}
+	}
+
+	if expectedCredential == nil {
+		log.Fatal("Credential not found")
+	}
+
+	fmt.Println(expectedCredential.GetContent())
+}
